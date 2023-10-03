7e76d5916399f61aa349df96a73566db30123397
hongtaiksu <hong2hwa58@gmail.com>
2023-10-03 23:10:30 +0900

    test

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CartController {
-aaaaa
+aaaaaaaa
     @Autowired
     private CartRepository cartRepository;
 
