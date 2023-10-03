1adb21a4da0d6162c0a05a6cd2d815a3de9e8970
hongtaiksu <hong2hwa58@gmail.com>
2023-10-04 00:03:21 +0900

    11111

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@aaController
+@Controller
 public class CartController {
 
     @Autowired
@@ -15,7 +15,7 @@ public class CartController {
 
     @GetMapping("/cart")
     public String cart() {
-        return "/cart";
+        return "/";
     }
 
     @GetMapping("/addToCart")
